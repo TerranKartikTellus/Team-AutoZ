a81689d2abb8fab456db8d8e6ecbf9bf91a62340
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2022-11-18 22:22:44 +0530

    questions

--- pages/recruitment/index.js
+++ pages/recruitment/index.js
@@ -1,70 +1,93 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HeadTag from "../../components/common/head";
 import Top from "../../components/common/top";
 import  Y from "/components/Animate/WhenInView/Translate/y"
 import Opacity from "/components/Animate/WhenInView/Opacity"
 import Scale from "/components/Animate/WhenInView/Scale"
-
+import {questions} from "/lib/data/recruitment"
 export default function Recruitment(){
-  let dat = [
-    {
-      name: "CSE",
-      description:"desc",
-      link:"/"
-    },
-    {
-      name: "ECE/EEE",
-      description:"desc",
-      link:"/"
-    },
-    {
-      name: "Mechanical",
-      description:"desc",
-      link:"/"
-    },
-    {
-      name: "Design and Social Media",
-      description:"desc",
-      link:"/"
-    },
-  ]
-  const [data,setData] = useState([dat[1],dat[2],dat[3]]);
-  const [active,setActive] = useState([0 , dat[0]]);
+
+  const [switchPanel, setSwitch] = useState(true);
+  useEffect(()=>{
+    document.getElementById("vid").addEventListener('ended', (event) => {
+      document.getElementById("vid").classList.add("hidden");console.log('end');
+    });
+  },[])
+  
   return(
-    <div className="bg-gra rounded-sm text-slate-100  bg-gray-50/0    select-none">
-      <HeadTag title="Recruitment | AutoZ" cardTitle="Recruitment | AutoZ" description="Research and Development team, VIT Vellore" image="" ></HeadTag>
+    <div className="bg-gra rounded-sm w-full text-slate-100  bg-gray-50/0  overflow-y-scroll  select-none overflow-y-hidden">
+      <div className="z-10">
+
+      <HeadTag className=""  title="Recruitment | AutoZ" cardTitle="Recruitment | AutoZ" description="Research and Development team, VIT Vellore" image="" ></HeadTag>
+      </div>
       <Top main="Recruitment " sub="AutoZ 2022-2023" mini={""}></Top>
 
 
-     <div className='h-screen snap-center pt-20  md:px-20 flex flex-col md:flex-row  items-center justify-center'>
-      {
-        active &&
-        <div onClick={()=>{
-          if(active[0]==3){
-                     setActive([0,dat[0]]);
-          }else          setActive([active[0]+1,dat[active[0]+1]]);
-        }} className="text-5xl scale-75 text-center bg-slate-50/50 h-[150px] w-[300px] md:p-0 p-2  md:h-4/6 flex flex-col items-center justify-center text-black md:w-1/2">
-          
-          <Opacity initial={0} final={1} duration={1/3} delay={1}>
-           {active[1].name}
-           <div className="text-lg">Tap to change Domain</div>
-          </Opacity>
-        </div>
-      } 
-      { data &&
-                <div className="w-full md:w-1/2 space-y-2  px-10 md:h-auto text-center md:text-left h-[350px]">
-                  <Opacity  initial={0} final={1} duration={1/3} delay={1}>
-                  <div className="space-y-2 w-full">
-
-                  <div className="text-4xl hidden md:block">{active[1].name}</div>
-                  <div className="text-xl">{active[1].description}</div>
-                  <div className="text-sm"><Link href={active[1].link}><a className="bg-gray-100/80 hover:bg-gray-100/70 rounded-sm p-2 text-black text-sm">View Document</a></Link></div>
-                  </div>
-                  </Opacity>
-                </div>
-      }
+     <div className='w-full h-screen snap-center pt-20  md:px-20 flex flex-col md:flex-row  items-center justify-center'>
+      {switchPanel && 
+      <div className="md:hidden"> 
+        <video controls autoPlay src="https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd" id="vid"  autoPlay   className="w-[400px]"   >
+         <source autoPlay src={"https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd"}  id="vid"
+          type="video/mp4" />No video found
+        </video>
+         </div>}
+      {<div className="w-full">{questions && <ShowQuestions q={questions}></ShowQuestions>}</div>}
      </div>
     </div>
+  );
+}
+
+function ShowQuestions({q}){
+  return(
+  <div className="flex flex-row items-center justify-center space-x-3">
+    <div className="w-1/2   h-screen  fixed  flex flex-col items-center justify-center top-0 left-0">
+    {/* <div className="hidden">  */}
+        <div className=" hidden md:block w-5/6 fenter">
+            <video controls   autoPlay loop src="https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd" id="vid"  className="w-full"   >
+             <source autoPlay src={"https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd"}  id="vid"
+              type="video/mp4" />No video found
+            </video>
+            
+          </div>
+    {/* </div> */}
+    </div>
+    <div className="w-1/2 hidden md:block">
+    {/* <div className="hidden">  */}
+        {/* <video controls autoPlay src="https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd" id="vid"  className="w-full"   >
+         <source autoPlay src={"https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd"}  id="vid"
+          type="video/mp4" />No video found
+        </video> */}
+    {/* </div> */}
+    </div>
+    <div className=" md:w-1/2 py-14 my-8 ">
+      <div className="overflo pt-[2900px] md:pt-[1800px] ">
+        <div className=" tracking-wider text-sm ml-5 ">Try today</div>
+        {q.map((i,index)=>{
+          return (
+            <div className="text-black z-50 tracking-wide md:bg-slate-50/80 bg-slate-50 hover:md:bg-slate-50/10 hover:md:text-white rounded-sm m-3 p-5 group block transition-all duration-500 ease-in-out" key={index}>
+            
+            <div className=" text-2xl fn font-medium pb-2"><strong className="tracking-wide opacity-90">Questions {index+1}: {i[0]}</strong></div>
+            <div className=" border-t-[1px] border-black py-1 "><strong>Skills required:</strong> {i[1]}</div>
+            <div className=" border-t-[1px] border-black py-1 ">
+              <strong>Difficulty level:</strong> {i[2]} 
+              
+            </div>
+          </div>)
+        })}
+        </div>
+    </div>
+  </div>
+  );
+}
+
+function HalfStar(){
+  return(
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 5.173l2.335 4.817 5.305.732-3.861 3.71.942 5.27-4.721-2.524v-12.005zm0-4.586l-3.668 7.568-8.332 1.151 6.064 5.828-1.48 8.279 7.416-3.967 7.416 3.966-1.48-8.279 6.064-5.827-8.332-1.15-3.668-7.569z"/></svg>
+  );
+}
+function Star(){
+  return(
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
   );
 }
