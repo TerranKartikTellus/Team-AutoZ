1bda0b0a4936660eb22887eb5b663a22b5b7b415
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2022-11-26 18:53:49 +0530

    added all questions for recruitment drive 2022-23

--- components/common/top.js
+++ components/common/top.js
@@ -4,7 +4,7 @@ import Opacity from "/components/Animate/WhenInView/Opacity"
 
 export default function Top({main,sub,mini}){
   return(
-    <main className="fixed md:top-14 md:left-14 top-5 left-auto right-auto  w-full scale-100 md:scale-100 ">
+    <main className="fixed md:top-14 md:left-14 top-5 left-auto right-auto  w-full scale-100 md:scale-100 z-10">
       <Y initial={'-20px'} final={'0px'} duration={1}>
         <Opacity  initial={0} final={1} duration={1}>
          <div className="text-3xl md:text-4xl  tracking-widest text-blue-100 w-[200px]  text-center mx-auto md:mx-0 md:text-left  bg-red300 font-medium border-b-2 border-gray-100/30 pb-3 mb-3">{main}</div>
@@ -14,7 +14,7 @@ export default function Top({main,sub,mini}){
       {sub && <div className=" bg-red400 w-full b-red-400 mx-auto md:mx-0">
       <X initial={'-20px'} final={'0px'} duration={1} delay={1}>
         <Opacity  initial={0} final={1} duration={1} delay={1}>
-         <div className="text-xl tracking-widest text-blue-100/80 text md:text-left mx-auto md:mx-0 w-[300px] text-center bg-red300 font-normal">{sub} </div>
+         <div  className="text-xl  tracking-widest text-blue-100/80 text md:text-left mx-auto md:mx-0 w-[300px] text-center bg-red300 font-normal">{sub} </div>
         </Opacity>
       </X>
 
--- pages/recruitment/index.js
+++ pages/recruitment/index.js
@@ -1,38 +1,50 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import React, { useRef, useMemo, Fragment } from 'react'
 import HeadTag from "../../components/common/head";
 import Top from "../../components/common/top";
-import  Y from "/components/Animate/WhenInView/Translate/y"
-import Opacity from "/components/Animate/WhenInView/Opacity"
-import Scale from "/components/Animate/WhenInView/Scale"
-import {questions} from "/lib/data/recruitment"
-export default function Recruitment(){
 
+import {questions} from "/lib/data/recruitment"
+import ReactStars from "react-rating-stars-component";
+
+export default function Recruitment(){
+  const [show,setshow] = useState(true);
+  console.log(show);
   const [switchPanel, setSwitch] = useState(true);
+  const myRef= useRef();
   useEffect(()=>{
     document.getElementById("vid").addEventListener('ended', (event) => {
       document.getElementById("vid").classList.add("hidden");console.log('end');
     });
+    const observer = new IntersectionObserver((entries)=>{
+      const entry = entries[0];
+      console.log('entry',entry);
+      setshow(!entry.isIntersecting);
+    })
+
+    observer.observe(myRef.current)
+    
   },[])
-  
+
+
   return(
-    <div className="bg-gra rounded-sm w-full text-slate-100  bg-gray-50/0  overflow-y-scroll  select-none overflow-y-hidden">
+    <div className="bg-gra  overflidden b400 md:h-full rounded-sm w-full text-slate-100  bg-gray-50/0  overflow-y-scroll  select-none overflow-y-hidden">
       <div className="z-10">
 
       <HeadTag className=""  title="Recruitment | AutoZ" cardTitle="Recruitment | AutoZ" description="Research and Development team, VIT Vellore" image="" ></HeadTag>
       </div>
-      <Top main="Recruitment " sub="AutoZ 2022-2023" mini={""}></Top>
+      {<Top main="Recruitment " sub="AutoZ 2022-2023" mini={""}></Top>}
 
 
      <div className='w-full h-screen snap-center pt-20  md:px-20 flex flex-col md:flex-row  items-center justify-center'>
-      {switchPanel && 
-      <div className="md:hidden"> 
-        <video controls autoPlay src="https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd" id="vid"     className="w-[400px]"   >
+      {/* {switchPanel && 
+      <div style={{ position: 'relative',zIndex:1}} className="mx-auto z-50 md:hidden"> 
+        <video style={{ position: 'relative',zIndex:1}} controls autoPlay src="https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd" id="vid"     className="w-[400px]"   >
          <source autoPlay src={"https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd"}  id="vid"
           type="video/mp4" />No video found
         </video>
-         </div>}
-      {<div className="w-full">{questions && <ShowQuestions q={questions}></ShowQuestions>}</div>}
+         </div>} */}
+      {<div ref={myRef} className="w-full">{questions && <ShowQuestions q={questions}></ShowQuestions>}</div>}
      </div>
     </div>
   );
@@ -43,36 +55,38 @@ function ShowQuestions({q}){
   <div className="flex flex-row items-center justify-center space-x-3">
     <div className="w-1/2   h-screen  fixed  flex flex-col items-center justify-center top-0 left-0">
     {/* <div className="hidden">  */}
-        <div className=" hidden md:block w-5/6 fenter">
+        <div className="text-center text-xl hidden md:block w-5/6 fenter">
             <video controls   autoPlay loop src="https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd" id="vid"  className="w-full"   >
              <source autoPlay src={"https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd"}  id="vid"
               type="video/mp4" />No video found
             </video>
-            
+          
           </div>
     {/* </div> */}
     </div>
     <div className="w-1/2 hidden md:block">
  
     </div>
-    <div className=" md:w-1/2 py-14 my-8 ">
-      <div className="overflo pt-[2900px] md:pt-[1800px] ">
-        <div className="md:hidden mb-10"> 
-        <video controls autoPlay src="https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd" id="vid"     className="w-[400px]"   >
+    <div className=" md:w-1/2 py-14 my-10 overflow-x-hidden">
+      <div className="mx-auto overflo pt-[5200px] md:pt-[2700px] ">
+        {/* <div className="rounded-md md:hidden mx-auto bed-400 mb-10"> 
+        <video className="rounded-md mx-auto" controls autoPlay src="https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd" id="vid"       >
          <source autoPlay src={"https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd"}  id="vid"
           type="video/mp4" />No video found
         </video>
-         </div>
-        <div className=" tracking-wider text-sm ml-5 ">Try today</div>
+         </div> */}
+        {/* <div className=" tracking-wider text-sm ml-5 ">Try today</div> */}
         {q.map((i,index)=>{
           return (
-            <div className="text-black z-50 tracking-wide md:bg-slate-50/80 bg-slate-50 hover:md:bg-slate-50/10 hover:md:text-white rounded-sm m-3 p-5 group block transition-all duration-500 ease-in-out" key={index}>
+            <div className="text-white z-50  tracking-wide   border-b  rounded-sm m-3 p-5 group block transition-all duration-500 ease-in-out" key={index}>
             
-            <div className=" text-2xl fn font-medium pb-2"><strong className="tracking-wide opacity-90">Questions {index+1}: {i[0]}</strong></div>
-            <div className=" border-t-[1px] border-black py-1 "><strong>Skills required:</strong> {i[1]}</div>
-            <div className=" border-t-[1px] border-black py-1 ">
-              <strong>Difficulty level:</strong> {i[2]} 
-              
+            <div className=" text-2xl fn font-medium pb-2     opacity-"><strong className="tracking-wide opacity-90">Questions {index+1}: {i[0]}</strong></div>
+            <div className="flex md:flex-row flex-col w-full md:items-center md:justify-around">
+            <div className="w-full md:w-8/12 border-t-[1p border-black py-1 opacity-80 tracking-wider"><strong>Skills required:</strong> {i[1]}</div>
+            <div className="w-full md:w-4/12 bg-re-400 text-center md:text-right border-t-[1p border-black py-1 opacity-80">
+              <strong>Difficulty level</strong> 
+              <div className=" flex flex-row items-center md:justify-end justify-center select-none">{<Stars number={i[2]}></Stars>}</div> 
+            </div>
             </div>
           </div>)
         })}
@@ -82,13 +96,18 @@ function ShowQuestions({q}){
   );
 }
 
-function HalfStar(){
+function Stars({number}){
   return(
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 5.173l2.335 4.817 5.305.732-3.861 3.71.942 5.27-4.721-2.524v-12.005zm0-4.586l-3.668 7.568-8.332 1.151 6.064 5.828-1.48 8.279 7.416-3.967 7.416 3.966-1.48-8.279 6.064-5.827-8.332-1.15-3.668-7.569z"/></svg>
-  );
-}
-function Star(){
-  return(
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
+    <ReactStars
+    count={5}
+    // onChange={ratingChanged}
+    size={24}
+    isHalf={true}
+    emptyIcon={<i className="far fa-star"></i>}
+    halfIcon={<i className="fa fa-star-half-alt"></i>}
+    fullIcon={<i className="fa fa-star"></i>}
+    activeColor="rgb(203 213 225)"
+    value={number}
+  />
   );
 }
