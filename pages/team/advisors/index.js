0767f13709dc5fe98d41999a95028b249f2f9daa
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2023-01-13 19:44:18 +0530

    feature added to profiles of all members

--- pages/team/advisors/index.js
+++ pages/team/advisors/index.js
@@ -48,7 +48,7 @@ export default function Team(){
                 <img src={i.img} className="w-full h-80 opacity-90 object-cover rounded-md shadow-lg "></img>
                 <div className="absolute top-0 pr-2 left-0 w-full h-full bg-black/20 flex flex-col items-end justify-end">
                   <div className="flex flex-row items-center justify-center space-x-1 scale-75">
-                     <Social google={i.email} linkdin={i.linkdin} insta={i.insta}></Social>
+                     <Social feature={i.feature} google={i.email} linkdin={i.linkdin} insta={i.insta}></Social>
                   </div>
                   {i.position && <div className=" opacity-80 overflow-hidden text-right font-thin tracking-wide">{i.position}</div>}
                   <div className="  text-xl font-medium text-right tracking-wider overflow-hidden mb-2">{i.name}</div>
@@ -73,7 +73,7 @@ function People(){
     </div>
   );
 }
-function Social({google,linkdin,insta}){
+function Social({feature,google,linkdin,insta}){
 
   return(
     <div className=' flex flex-row items-end justify-end space-x-2 opacity-80  text-gray-100 w-full '>
@@ -100,6 +100,16 @@ function Social({google,linkdin,insta}){
           <RX initial={"90deg"} final={"0deg"} duration={0.6} delay={2.2}>
           <Link href={insta}>
           <a target="_blank" className='opacity-80 transition-all duration-300 ease-in-out hover:opacity-90 '><svg className='invert  w-8 h-8' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14.829 6.302c-.738-.034-.96-.04-2.829-.04s-2.09.007-2.828.04c-1.899.087-2.783.986-2.87 2.87-.033.738-.041.959-.041 2.828s.008 2.09.041 2.829c.087 1.879.967 2.783 2.87 2.87.737.033.959.041 2.828.041 1.87 0 2.091-.007 2.829-.041 1.899-.086 2.782-.988 2.87-2.87.033-.738.04-.96.04-2.829s-.007-2.09-.04-2.828c-.088-1.883-.973-2.783-2.87-2.87zm-2.829 9.293c-1.985 0-3.595-1.609-3.595-3.595 0-1.985 1.61-3.594 3.595-3.594s3.595 1.609 3.595 3.594c0 1.985-1.61 3.595-3.595 3.595zm3.737-6.491c-.464 0-.84-.376-.84-.84 0-.464.376-.84.84-.84.464 0 .84.376.84.84 0 .463-.376.84-.84.84zm-1.404 2.896c0 1.289-1.045 2.333-2.333 2.333s-2.333-1.044-2.333-2.333c0-1.289 1.045-2.333 2.333-2.333s2.333 1.044 2.333 2.333zm-2.333-12c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.958 14.886c-.115 2.545-1.532 3.955-4.071 4.072-.747.034-.986.042-2.887.042s-2.139-.008-2.886-.042c-2.544-.117-3.955-1.529-4.072-4.072-.034-.746-.042-.985-.042-2.886 0-1.901.008-2.139.042-2.886.117-2.544 1.529-3.955 4.072-4.071.747-.035.985-.043 2.886-.043s2.14.008 2.887.043c2.545.117 3.957 1.532 4.071 4.071.034.747.042.985.042 2.886 0 1.901-.008 2.14-.042 2.886z"/></svg></a>
+          </Link>
+          </RX>
+        </div>
+        }
+        {feature && <div>
+          <RX initial={"90deg"} final={"0deg"} duration={0.6} delay={2.2}>
+          <Link href={feature}>
+          <a target="_blank" className='opacity-80 w-8 h-8 bg-gray-100 flex flex-row items-center justify-center rounded-full transition-all duration-300 ease-in-out hover:opacity-90 '>
+            <svg className=' fill-black w-full pt-1 pl-1 mx-auto bg-gray-100 h-full rounded-full' width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M16 3.383l-.924-.383-7.297 17.617.924.383 7.297-17.617zm.287 3.617l6.153 4.825-6.173 5.175.678.737 7.055-5.912-7.048-5.578-.665.753zm-8.478 0l-6.249 4.825 6.003 5.175-.679.737-6.884-5.912 7.144-5.578.665.753z"/></svg>
+            </a>
           </Link>
           </RX>
         </div>
